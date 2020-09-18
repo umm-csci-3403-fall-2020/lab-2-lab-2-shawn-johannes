fa936919dfdd37461c07bf51c9bb8948899ff3a6
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-18 01:11:36 -0500

    Free memory and give user an option to quit the program

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -9,8 +9,15 @@ int main(int argc, char *argv[]) {
   
   size = 100;
   line = (char*) malloc (size + 1);
+  
+  printf("Enter a phrase to disemvowel. Enter q to quit \n");
 
-  while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+  while (getline(&line, &size, stdin) > 0 && line[0] != 'q') {
+    char *disemvowelResult = disemvowel(line);
+    printf("%s\n", disemvowelResult);
+    free(disemvowelResult);
   }
+
+  
+  free(line);
 }
