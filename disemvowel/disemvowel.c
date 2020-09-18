4920bf6e550d456b734f17498598add4c9a54dcf
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-18 00:53:00 -0500

    Write detect_vowel function
    Comprised of a switch statement that lists vowels as cases, the detect_vowel function returns 1 (true) if a vowel is sent to it, and 0 (false) if not.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,28 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+int detect_vowel(char currentCharacter) {
+  switch(currentCharacter) {
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+      return 1;
+    default:
+      return 0;
+  }
 }
+
+          
+  
+
+    
+			
